e9ae85f1a9ea27938a977ef8e9fd5063ff26185e
monginadiana <dianahmongina2@gmail.com>
2022-09-12 11:24:42 +0300

    push code

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const timeOut= setTimeout(()=> setTimeRemaining(timeRemaining -1), 1000)
+    return function cleanup() {
+      clearInterval(timeOut);
+    };
 
+    }, [])
+
+
+  if (timeRemaining === 0){
+    setTimeRemaining(10)
+    onAnswered(false)
+
+  }
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
